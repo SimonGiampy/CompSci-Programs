1c639adcf9b9d7cea5f684e9035c917a770c5ae9
Simon Giampy <simongiampydev@gmail.com>
2018-10-23 09:35:19 +0200

    added new exercise of palyndrome strings

---
+++ palyndrome.c
@@ -0,0 +1,13 @@
+/*
+   legge in input una stringa e scrive se è palindroma o no
+ */
+#include <stdio.h>
+
+int main() {
+	char string[100];
+	scanf("%s", string); //in realtà questa scanf prende l'input fino alla pressione di uno spazio
+	//accetta infatti una sola parola. per accettare una frase intera, bisogna scrivere scanf("s[^\n]", string);
+
+
+	return 0;
+}
