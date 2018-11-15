44d87c867d58d3dafb5723112ad620e6a8920964
SimonGiampy <simongiampydev@gmail.com>
2018-11-15 16:59:08 +0100

    modified bubble sort

--- bubble-sort.c
+++ bubble-sort.c
@@ -5,9 +5,10 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define X 10
+#define X 20
 
 void swap(int *a, int *b);
+void printAllOfThem(int numbers[]);
 
 int main() {
 	int numbers[X];
@@ -16,22 +17,30 @@ int main() {
 	printf("sequence: ");
 	for (int i = 0; i < X; i++) {
 		numbers[i] = rand()%100 + 1;
-		printf("%d   ", numbers[i]);
 	}
-	printf("\n");
+
+	printAllOfThem(numbers);
+
+	int swapped = 0, stop = 0;
 
 	printf("swapped sequence: ");
-	for (int j = 0; j < X-2; j++) {
+	//for (int j = 0; j < X-2; j++) {
+	while (stop == 0) {
 		for (int i = 0; i < X -1; i++) {
-			if (numbers[i] > numbers[i+1]) {
-				swap(&numbers[i], &numbers[i+1]);
+			if (*(numbers+i) > *(numbers+i+1)) {
+				swap((numbers+i), (numbers+i+1));
+				swapped = 1;
+			} else {
+				stop += 1;
 			}
-			printf("%d   ", numbers[i]);
 		}
-		printf("\n");
+		if (swapped == 1) {
+			stop = 0;
+			swapped = 0;
+		}
 	}
-	printf("\n");
-
+	//}
+	printAllOfThem(numbers);
 
 	return 0;
 }
@@ -41,4 +50,11 @@ void swap(int *a, int *b) {
 	temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+void printAllOfThem(int numbers[]) {
+	for (int i = 0; i < X; i++) {
+		printf("%d   ", *(numbers + i));
+	}
+	printf("\n");
 }
